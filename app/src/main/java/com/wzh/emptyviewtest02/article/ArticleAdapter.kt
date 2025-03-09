a39763ecb1a32370f8f934e7b71f8b392c1bd0f5
freddywang <2215991508@qq.com>
2025-03-09 10:38:44 +0800

    另两个支线完成，但是跑不起来

--- app/src/main/java/com/wzh/emptyviewtest02/article/ArticleAdapter.kt
+++ app/src/main/java/com/wzh/emptyviewtest02/article/ArticleAdapter.kt
@@ -128,7 +128,7 @@ class ArticleAdapter(
                     mContext.showToast(mContext.getString(R.string.no_network))
                     return@setOnClickListener
                 }
-                ArticleActivity.actionStart(mContext, data)
+//                ArticleActivity.actionStart(mContext, data)
                 val browseHistoryDao = PlayDatabase.getDatabase(mContext).browseHistoryDao()
                 launch(Dispatchers.IO) {
                     if (browseHistoryDao.getArticle(data.id, HISTORY) == null) {
